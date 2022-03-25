53585ace84e708a957cf49655285a8ff94763368
Evan Coleman <evanacoleman@gmail.com>
2022-03-25 12:33:06 -0700

    Update information and style for site

--- scripts/methods.js
+++ scripts/methods.js
@@ -13,12 +13,10 @@ function onWindowResize() {
 
 
   if(window.mobileCheck()) {
-    $("#eac-nav").height('7%');
+    //$("#eac-nav").height('7%');
     $("#eac-nav .eac-name-container p").css("font-size", ($("#eac-nav").height()*0.5) + 'px');
-    $("#eac-nav .eac-name-container p").css("top", '-40%');
-    $("#eac-nav .eac-name-container p#slash").css("font-size", ($("#eac-nav").height()*2) + 'px');
-    $("#eac-nav .eac-name-container p#slash").css("top", -($("#eac-nav").height()*0.5) + 'px');
-
+    //$("#eac-nav .eac-name-container p").css("top", '-40%');
+    
     $("#eac-body .eac-content-container").css("font-size",'40px');
     $("#eac-body .eac-content-container .eac-header").height('30%');
     $(".eac-content-container").css("top",$("#eac-nav").height() + 'px');
@@ -28,150 +26,8 @@ function onWindowResize() {
   };
 
   // home tab: make sure the body padding centers the text properly
-  $("#HOME.eac-content .eac-content-container").css("top",$("#eac-nav").height() + 'px');
-  $("#HOME.eac-content .eac-content-body").height(($("#HOME.eac-content").height() - $("#HOME.eac-content .eac-header").height() - 
-                                          $("#eac-nav").height() - 40) + 'px');
-
-  $("#eac-body .eac-content-container").height( ($("#eac-body").height() - $("#eac-nav").height()) + 'px');
-  $("#eac-body .eac-content-container").width( ($("#eac-body").width()) + 'px');
+  $("#eac-body .eac-content-container").width( ($("#eac-body").width() -0) + 'px');
   $("#eac-body .eac-content-container .eac-content-body").width( ($("#eac-body .eac-content-container").width() - 60) + 'px');
-  $("#eac-body .eac-content-container .eac-content-body").height( ($("#eac-body .eac-content-container").height() - $("#eac-body .eac-content-container .eac-header").height() - 41) + 'px');
-
-
-  // work tab: "under construction" line should be centered
-  //$("#WORK.eac-content > span").css('top', ($("#WORK.eac-content").height()/2 - 26) + 'px');
-
-  //contact tab: contact info should be centered
-  //$("#CONTACT.eac-content .eac-sidebar").css('top', 
-  //                                             ($("#CONTACT.eac-content").height()/2 - 150) 
-  //                                                + 'px');
-}
-
-function addWork(title, imageSrc, url) {
-  // i'm lazy, add in a work div for me
-  $("#WORK.eac-content").append('<div name="'+title+'" class="eac-work" '
-                                  + 'style="background-image: url('+imageSrc+')">'
-                                  + '<span>'+title+'</span></div>');
-
-  // open url on click
-  $("#WORK.eac-content div.eac-work[name='"+title+"']").click(function() {
-    window.open(url);
-  });
-}
-
-function addWorkFolder(title, imageSrc, json) {
-  // i'm lazy, add in a work div for me
-  $("#WORK.eac-content").append('<div name="'+title+'" class="eac-work" '
-                                  + 'style="background-image: url('+imageSrc+')">'
-                                  + '<span>'+title+'</span></div>'
-                                  + '<div name="'+title+'" class="eac-work-folder"></div>');
-
-  // open url on click
-  $("#WORK.eac-content div.eac-work[name='"+title+"']").click(function() {
-    thisIsOpen = $("#WORK.eac-content div.eac-work-folder[name='"+title+"']");
-    thisIsOpen = thisIsOpen.css("display") == "block";
-
-    blurs= thisIsOpen ? null : $("#WORK.eac-content > div.eac-work[name!='"+title+"']");
-    
-    $("#WORK.eac-content > div.eac-work").animate({
-      opacity: 1
-    },{
-      duration: 50,
-      queue: false,
-      easing: "easeOutQuad"
-    });
-
-    if(blurs) {
-      blurs.animate({
-        opacity: 0.6
-      },{
-        duration: 50,
-        queue: false,
-        easing: "easeOutQuad"
-      });
-    }
-
-/*
-    blurs=$("#WORK.eac-content > div.eac-work[name!='"+title+"']").filter(function(){
-      work=$("#WORK.eac-content > div.eac-work-folder[name='"+$(this).attr("name")+"']")
-      if(work == null || work.length==0) return true;
-      else if(work.css("display")=="block") return false;
-      else return true;
-    });
-
-    unblurs=$("#WORK.eac-content > div.eac-work[name!='"+title+"']").filter(function(){
-      work=$("#WORK.eac-content > div.eac-work-folder[name='"+$(this).attr("name")+"']")
-      if(work == null || work.length==0) return false;
-      else if(work.css("display")=="block") return true;
-      else return false;
-    });
-
-    if(thisIsOpen && unblurs.length<=1) { 
-      unblurs.add(this).add(blurs); 
-      blurs=null;
-    } else if(thisIsOpen && unblurs.length>1){
-      blurs.add(this);
-    } else if(!thisIsOpen) {
-      unblurs.add(this);
-    }
-
-    if(blurs) {
-      blurs.animate({
-        opacity: 0.6
-      },{
-        duration: 50,
-        queue: false,
-        easing: "easeOutQuad"
-      }).addClass("blurred");
-    }
-
-    unblurs.animate({
-      opacity: 1
-    },{
-      duration: 50,
-      queue: false,
-      easing: "easeOutQuad"
-    }).removeClass("blurred");
-*/
-  
-    $("#WORK > .eac-work-folder[name!='"+title+"']").slideUp({
-      duration: 250,
-      queue: false,
-      easing: "easeOutQuad"
-    });  
-
-    $("#WORK > .eac-work-folder[name='"+title+"']").slideToggle({
-      duration: 500,
-      queue: false,
-      easing: "easeOutQuad"
-    });
-
-  });
-  
-  $("#WORK > .eac-work-folder").each(function(i,thing){
-    modIndex 
-      = $("#WORK > .eac-work,.eac-work-folder[name='"+$(thing).attr("name")+
-            "']").index($("#WORK .eac-work-folder[name='"+
-            $(thing).attr("name")+"']"));
-    modIndex = Math.ceil(modIndex/5) *5 - 1;
-
-
-    $(thing).insertAfter($("#WORK > .eac-work").eq(modIndex));
-  });
-
-  jQuery.each(json, function(i,val) {
-    $("#WORK.eac-content .eac-work-folder[name='"+title+"']").append(
-                                '<div name="'+i+'" class="eac-work" '
-                                    + 'style="background-image: url('+val[0]+')">'
-                                    + '<span>'+i+'</span></div>');
-    $("#WORK.eac-content .eac-work-folder[name='"+title+"'] "
-      + "div.eac-work[name='"+i+"']").click(
-        function() {
-          window.open(val[1]);
-        }
-    );
-
-  });
 }
 
 function navComplete() { 
@@ -223,8 +79,7 @@ function navComplete() {
 
 $(window).load(function() {
   // make the scrollbar in the home window, work window
-  $("#CV").perfectScrollbar();
-  $("#WORK").perfectScrollbar();
+  //$("#CV").perfectScrollbar();
 
   // make sure the content body has the proper width
   $(".eac-content-body").width(($(".eac-content-container").width() - 60) + 'px');
@@ -232,20 +87,6 @@ $(window).load(function() {
   // add onWindowResize to event stack
   onWindowResize();
   window.addEventListener( 'resize', onWindowResize, false );
-
-  // make the zippity zoom nav helper come out of hiding on load
-  /*$(".eac-nav-helper").animate({
-    width: $(".eac-nav-button.selected").children('span').width() + 9,
-    marginLeft: "-=" + ($('.eac-nav-helper').offset().left 
-                        - $('.eac-nav-button.selected').offset().left 
-                        - 14)
-  }, {
-    duration: 1500,
-
-    queue: false,
-    complete: navComplete,
-    easing: "easeOutQuart"
-  });*/
 
   // home window opacity animates to 1 on load
   $(".eac-content[id='HOME']").animate({
@@ -255,49 +96,5 @@ $(window).load(function() {
      queue: false,
      easing: "easeOutQuad"
    });
-
-  addWork("Cubiculo", "images/Cubiculo.jpg", "http://evan-a-coleman.com/4D/");
-
-  //addWorkFolder("(Underclassman) <br> Course notes", "", 
-  //{
-  //  "PHYS1410: Quantum Mechanics Notes":  ["", "./Documents/PHYS1410/PHYS1410_Notes.pdf"],
-  //  "MATH1130: Multivariate Analysis Midterm II Study Guide":   ["", "./Documents/MATH1130/MATH1130_SG2.pdf"],
-  //  "MATH1010: Analysis Midterm II Study Guide":   ["", "./Documents/MATH1010/MATH1010_SG2.pdf"],
-  //  "MATH0540: Linear Algebra Midterm II Study Guide":   ["", "./Documents/MATH0540/MATH0540_SG2.pdf"]
-  //});
-
-  //addWorkFolder("PHYS2470: Renormalization", "", 
-  //{
-  //  //"Set 1":                    ["", "http://evan-a-coleman.com/"],
-  //  //"Set 2":                    ["", "http://evan-a-coleman.com/"],
-  //  "Set 3 (Used as solution)": ["", "./Documents/PHYS2470/Set3.pdf"],
-  //  //"Set 4":                    ["", "http://evan-a-coleman.com/"],
-  //  "Set 5 (Used as solution)": ["", "./Documents/PHYS2470/Set5.pdf"],
-  //  //"Set 6":                    ["", "http://evan-a-coleman.com/"],
-  //});
-
-  addWorkFolder("13 TeV Top Width Measurement", "", 
-  {
-    "CERN Document Server": ["", "http://cds.cern.ch/record/2218019"],
-    "InspireHEP":           ["", "http://inspirehep.net/record/1487815"],
-    "TOP2016 Poster":       ["", "./Documents/TopWidth/ColemanE_TopWidth_TOP2016.pdf"],
-  });
-
-  addWorkFolder("Track Observables Study", "", 
-  {
-    "CERNWeb & Plots": ["", "http://ecoleman.web.cern.ch/ecoleman/TrackObservablesStudy/"],
-    "BOOST2017 Poster": ["", "./Documents/TrackObservablesStudy/ColemanE_TrackObservables_BOOST2017"],
-    "arXiv": ["", "https://arxiv.org/abs/1709.08705"]
-  });
-
-  addWork("Github",   "", "https://www.github.com/eacoleman/");
-  addWork("Lectures",   "", "https://www.youtube.com/channel/UCzKSkbluN42ui-rxH_WHxDQ?view_as=subscriber");
-
-  addWorkFolder("(Upperclassman) <br> Course notes", "", 
-  {
-    "PHYS2300: Quantum Field Theory I Final Project, The Axial Anomaly": ["","./Documents/PHYS2300/Final Project.pdf"],
-    "Hausdorff-Ising Model":                                             ["", "./Documents/ColemanE_HausdorffIsingModel.pdf"],
-    "The Lie Algebra of <br> SU ( 2 to the M )":                         ["", "./Documents/ColemanE_LieAlgebraSU(2toM).pdf"]
-  });
 
 });
